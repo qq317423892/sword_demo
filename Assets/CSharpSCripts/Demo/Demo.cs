02bb553f7f6a29d6f49a0178aa2fed4ff43ab247
moon <vincent.moon@moondeMacBook-Pro.local>
2020-04-24 22:34:00 +0800

    【开发】增加动画文件

--- Assets/CSharpSCripts/Demo/Demo.cs
+++ Assets/CSharpSCripts/Demo/Demo.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Demo : MonoBehaviour {
+public class Demo : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+        {
+           
+        }
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other != null)
+        {
+            Debug.Log(">>>>>>>>>>> OnTriggerStay" + other.ToString());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other != null)
+        {
+            Debug.Log(">>>>>>>>>>> OnTriggerExit" + other.ToString());
+        }
+    }
 }
+
